ba0e24ad4302cbcebbfdc753acb01504f88c1b4c
Roszczyk <mikolaj.roszczyk.stud@pw.edu.pl>
2023-12-19 11:36:55 +0100

    GenTest - zadanie 5

---
+++ w7/genTest.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import numpy as np 
+import sys
+import os
+from testrunner import run
+
+OUTSIDE=0
+BEFORE=1
+AFTER=2
+
+def chooseBrackets():
+    testing_brackets=["{", "}", "[", "]", "(", ")"]
+    choose_brackets=np.random.randint(0,2)
+
+    return [testing_brackets[choose_brackets*2], testing_brackets[choose_brackets*2+1]]
+
+
+
+def genTest():
+    testing_positions=[OUTSIDE, BEFORE, AFTER]
+    choose_positions=np.random.randint(0,2)
+
+    testing_brackets=chooseBrackets()
+
+    if choose_positions==OUTSIDE:
+        string=f"{testing_brackets[0]}StringTest{testing_brackets[1]}"
+    if choose_positions==BEFORE:
+        string=f"{testing_brackets[0]}{testing_brackets[1]}StringTest"
+    if choose_positions==AFTER:
+        string=f"StringTest{testing_brackets[0]}{testing_brackets[1]}"
+
+    return string
+
+def genIll():
+    correct=genTest()
+    option=np.random.randint(0,4)
+    usedbr=chooseBrackets()
+
+    if option==0:
+        string = correct + usedbr[1]
+    if option==1:
+        string.lstrip(correct[0])
+    if option==2:
+        string.rstrip(correct[len(correct)-1])
+    if option==3:
+        string = correct + usedbr[0]
+    if option==4:
+        string = usedbr[0] + correct
+    
+    return string
+
+def testfunc(child):
+    for i in range(100):
+        child.sendline(genTest())
+        child.expect_exact("GOOD")
+    for i in range(100):
+        child.sendline(genIll())
+        child.expect_exact("BAD")
+
+
+if __name__ == "__main__":
+    sys.exit(run(testfunc))
--- w7/generate_test_strings.py
+++ w7/generate_test_strings.py
@@ -4,28 +4,29 @@ import numpy as np
 def generate():
     test_strings=[]
     testing_brackets=["{", "}", "[", "]", "(", ")"]
-    for b in range(3):
-        good=[]
-        for j in range(10):
-            before=np.random.randint(0,5)
-            after=np.random.randint(0,5)
+    for ile in range (3):
+        for b in range(3):
+            good=[]
+            for j in range(10):
+                before=np.random.randint(0,5)
+                after=np.random.randint(0,5)
 
-            if before == after:
-                good.append("good")
-            else:
-                good.append("bad")
+                if before == after:
+                    good.append("good")
+                else:
+                    good.append("bad")
 
-            string="pair_brackets "
+                string="pair_brackets "
 
-            for i in range(before):
-                string = string+f"{testing_brackets[b*2]}"
+                for i in range(before):
+                    string = string+f"{testing_brackets[b*2]}"
 
-            string=string+"test"
+                string=string+"test"
 
-            for i in range(after):
-                string = string+f"{testing_brackets[b*2+1]}"
+                for i in range(after):
+                    string = string+f"{testing_brackets[b*2+1]}"
 
-            test_strings.append(string)
+                test_strings.append(string)
     
     return test_strings
 
